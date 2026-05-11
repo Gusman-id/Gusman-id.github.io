bdb659c0a3a5bd4758e206047185adde182c962a
Gusman-id <gusman.id@proton.me>
2026-05-11 11:16:14 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.style.display =
-    navLinks.style.display === "flex" ? "none" : "flex";
-});
